76f983a0b00688fc821fc4a7baf43c49a5a21a83
Hüseyin <yigit.hsynm@gmail.com>
2018-06-03 19:33:57 +0300

    login servise devredildi.

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 var express = require('express');
-var bodyParser = require("body-parser")
-var cors = require("cors")
 
-var request = require("request")
+var request = require('request');
 
 var passport = require('passport');
-var passStrategyLocal = require('passport-local').Strategy;
+
+var session = require('express-session');
+var mongodbSessionStore = require('connect-mongodb-session')(session);
 
 // Module related packages
-var MongoClient = require("mongodb").MongoClient
-var MongoObjectID = require('mongodb').ObjectID;
-
-var mongodb_url = "mongodb://127.0.0.1:27017"
+var mongoClient = require("mongodb").MongoClient
+var mongodbUrl = "mongodb://127.0.0.1:27017"
 
 // Create a new Express application.
 var app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+var store = new mongodbSessionStore({
+  uri: mongodbUrl,
+  databaseName: 'auth',
+  collection: 'sessions'
+});
+
+// Catch errors
+store.on('error', function (error) {
+  assert.ifError(error);
+  assert.ok(false);
+});
+
+app.use(require('express-session')({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  // Boilerplate options, see:
+  // * https://www.npmjs.com/package/express-session#resave
+  // * https://www.npmjs.com/package/express-session#saveuninitialized
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(require('morgan')('tiny'));
+app.use(require('body-parser').json())
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require("cors")())
 app.use("/favorites/bower_components", express.static(__dirname + "/public/bower_components"))
-app.use("/bower_components", express.static(__dirname + "/public/bower_components"))
 
 //==================================================================================================
 // Local Passport
@@ -31,18 +54,6 @@ app.use("/bower_components", express.static(__dirname + "/public/bower_component
 // (`username` and `password`) submitted by the user.  The function must verify
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
-passport.use(new passStrategyLocal(function (username, password, cb) {
-  request({
-    url: "http://127.0.0.1:3000/mongodb/api/auth/users?username=" + username + "&password=" + password,
-    headers: { "Authorization": "Bearer %Sdf1234" }
-  }, function (err, res, body) {
-    var users = JSON.parse(body)
-    if (err) return cb(err)
-    if (!users.length) { return cb(null, false); }
-    return cb(null, users[0]);
-  })
-}));
-
 
 // Configure Passport authenticated session persistence.
 //
@@ -56,56 +67,24 @@ passport.serializeUser(function (user, cb) {
 });
 
 passport.deserializeUser(function (username, cb) {
-  request({
-    url: "http://127.0.0.1:3000/mongodb/api/auth/users?username=" + username,
-    headers: { "Authorization": "Bearer %Sdf1234" }
-  }, function (err, res, body) {
-    var users = JSON.parse(body)
-    if (err) return cb(err)
-    if (!users.length) { return cb(null, false); }
-    return cb(null, users[0]);
-  })
+  mongoClient.connect(mongodbUrl + "/auth", function (err, db) {
+    db.collection("users").findOne({ username: username }, function (err, user) {
+      if (err) return cb(err)
+      if (!user) { return cb(null, false); }
+      return cb(null, user);
+      db.close();
+    });
+  });
 });
-
-
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/favorites/login', function (req, res) {
-  res.sendFile(__dirname + '/public/login.html');
-});
 
-app.post('/favorites/login', passport.authenticate('local', { failureRedirect: '/favorites/login', successRedirect: '/favorites' }), function (req, res) {
-  res.redirect('/favorites');
-});
-
-app.get('/favorites/logout', function (req, res) {
-  req.logout();
-  res.redirect('/favorites');
-});
-
-
-app.get('/favorites', require('connect-ensure-login').ensureLoggedIn({ redirectTo: "/favorites/login" }), function (req, res) {
+app.get('/favorites', require('connect-ensure-login').ensureLoggedIn({ redirectTo: "/login?source=favorites" }), function (req, res) {
   res.sendFile(__dirname + '/public/index.html')
-});
-
-app.get('/favorites/user', require('connect-ensure-login').ensureLoggedIn(), function (req, res) {
-  request({
-    url: "http://127.0.0.1:3000/mongodb/api/auth/users?token=" + req.user.token,
-    headers: { "Authorization": "Bearer %Sdf1234" }
-  }, function (err, res, body) {
-    var users = JSON.parse(body)
-    return cb(null, users[0]);
-  })
 });
 
 
